7fd3012187a3753e77d1475888009c230bc1bf1e
Nucleon <177419046+nucleon9999@users.noreply.github.com>
2025-01-14 01:01:40 -0500

    .

--- csc_build.ps1
+++ csc_build.ps1
@@ -1,4 +1,4 @@
-dotnet publish app.csproj 
+dotnet publish -c Release -r win-x64 /p:PublishSingleFile=true
 
 
 #Compress-Archive -Path ../bin/RoblosMultis.exe -DestinationPath ../bin/RoblosMultis.zip -Force
