4a8998fa04b69c2a221c981b6294fd4f02d9717f
Nucleon <177419046+nucleon9999@users.noreply.github.com>
2025-01-14 00:14:05 -0500

    .

--- src/csc_build.ps1
+++ src/csc_build.ps1
@@ -1,2 +1,2 @@
 dotnet publish app.csproj -c Release -o ../bin -r win-x64 /p:PublishSingleFile=true /p:DebugType=None --no-self-contained
-#Compress-Archive -Path ../bin/RoblosMultis.exe -DestinationPath bin/RoblosMultis.zip -Force
+Compress-Archive -Path ../bin/RoblosMultis.exe -DestinationPath ../bin/RoblosMultis.zip -Force
